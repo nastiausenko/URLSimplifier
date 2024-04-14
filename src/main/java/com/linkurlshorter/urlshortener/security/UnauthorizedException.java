45ee8750ef2dc5e72c80bbc126df3fc4833070ce
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-04-14 18:10:46 +0300

    added ForbiddenException and UnauthorizedException

---
+++ src/main/java/com/linkurlshorter/urlshortener/security/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.linkurlshorter.urlshortener.security;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
