2ef0d81836aa1df69c59e72965094d9e6d178d63
EgorSivenko <egor.siv20@gmail.com>
2024-04-17 00:10:43 +0300

    Implemented the base version of the link redirect controller

---
+++ src/main/java/com/linkurlshorter/urlshortener/redirect/LinkRedirectController.java
@@ -0,0 +1,33 @@
+package com.linkurlshorter.urlshortener.redirect;
+
+import com.linkurlshorter.urlshortener.link.Link;
+import com.linkurlshorter.urlshortener.link.LinkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class LinkRedirectController {
+
+    private final LinkService linkService;
+
+    @GetMapping("/{shortLink}")
+    public RedirectView redirectToLongLink(@PathVariable("shortLink") String shortLink) {
+        Link byShortLink = linkService.findByShortLink(shortLink);
+        byShortLink.setStatistics(byShortLink.getStatistics() + 1);
+        byShortLink.setExpirationTime(LocalDateTime.now().plusMonths(1));
+
+        String longLink = byShortLink.getLongLink();
+
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(longLink);
+        redirectView.setStatusCode(HttpStatusCode.valueOf(302));
+        return redirectView;
+    }
+}
