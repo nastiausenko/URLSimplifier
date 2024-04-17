00db751777ddf8ccb98a258836b488ee04dc3fb6
EgorSivenko <egor.siv20@gmail.com>
2024-04-17 11:00:11 +0300

    Refactor: added javadoc in link redirect controller

--- src/main/java/com/linkurlshorter/urlshortener/redirect/LinkRedirectController.java
+++ src/main/java/com/linkurlshorter/urlshortener/redirect/LinkRedirectController.java
@@ -11,12 +11,33 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.LocalDateTime;
 
+/**
+ * Controller class for handling link redirection requests.
+ *
+ * <p>This class provides an endpoint for redirecting short links to their corresponding long links.
+ * When a request is made with a short link, the controller fetches the corresponding long link
+ * from the database using the {@link LinkService#findByShortLink(String)}, updates link statistics,
+ * and redirects the user to the long link.
+ * Additionally, it updates the link's expiration time to extend its validity.
+ *
+ * @author Egor Sivenko
+ * @see org.springframework.web.servlet.view.RedirectView
+ */
 @RestController
 @RequiredArgsConstructor
 public class LinkRedirectController {
 
+    /**
+     * The service responsible for managing links
+     */
     private final LinkService linkService;
 
+    /**
+     * Redirects a request with a short link to its corresponding long link.
+     *
+     * @param shortLink the short link to be redirected
+     * @return a RedirectView object directing the user to the long link
+     */
     @GetMapping("/{shortLink}")
     public RedirectView redirectToLongLink(@PathVariable("shortLink") String shortLink) {
         Link link = linkService.findByShortLink(shortLink);
