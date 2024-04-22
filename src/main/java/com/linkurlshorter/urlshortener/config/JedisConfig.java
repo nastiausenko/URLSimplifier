c8ed1a4caff8ae356bbd92d9e33bf23c401ccc8c
EgorSivenko <egor.siv20@gmail.com>
2024-04-22 16:10:55 +0300

    Refactor: added javadoc in `JedisConfig`

--- src/main/java/com/linkurlshorter/urlshortener/config/JedisConfig.java
+++ src/main/java/com/linkurlshorter/urlshortener/config/JedisConfig.java
@@ -8,6 +8,19 @@ import redis.clients.jedis.JedisPoolConfig;
 
 import java.time.Duration;
 
+/**
+ * Configuration class for setting up Jedis, a Java Redis client.
+ *
+ * <p>This class configures a JedisPool bean, which serves as a connection pool to manage connections
+ * to the Redis server. It specifies the Redis server host and port using properties defined in
+ * the application.properties file. Additionally, it configures various connection pool parameters
+ * such as maximum total connections, maximum idle connections, and eviction settings to optimize
+ * connection management and performance.
+ *
+ * @author Egor Sivenko
+ * @see redis.clients.jedis.JedisPool
+ * @see redis.clients.jedis.JedisPoolConfig
+ */
 @Configuration
 public class JedisConfig {
 
@@ -17,11 +30,21 @@ public class JedisConfig {
     @Value("${REDIS_PORT:0}")
     private int port;
 
+    /**
+     * Creates and configures a JedisPool bean for managing Redis connections.
+     *
+     * @return the configured JedisPool bean
+     */
     @Bean
     public JedisPool jedisPool() {
         return new JedisPool(buildPoolConfig(), host, port);
     }
 
+    /**
+     * Builds and configures the JedisPoolConfig for the JedisPool bean.
+     *
+     * @return the configured JedisPoolConfig object
+     */
     private JedisPoolConfig buildPoolConfig() {
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(128);
