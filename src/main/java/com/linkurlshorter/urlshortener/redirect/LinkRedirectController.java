982ee318b1197afe982e8a3b405bda2802a21fa3
EgorSivenko <egor.siv20@gmail.com>
2024-04-23 12:32:20 +0300

    Moved and updated redirect logic to the link service

--- src/main/java/com/linkurlshorter/urlshortener/redirect/LinkRedirectController.java
+++ src/main/java/com/linkurlshorter/urlshortener/redirect/LinkRedirectController.java
@@ -1,10 +1,7 @@
 package com.linkurlshorter.urlshortener.redirect;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.linkurlshorter.urlshortener.link.Link;
 import com.linkurlshorter.urlshortener.link.LinkService;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatusCode;
@@ -12,19 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-
-import java.time.LocalDateTime;
 
 /**
  * Controller class for handling link redirection requests.
  *
  * <p>This class provides an endpoint for redirecting short links to their corresponding long links.
- * It first checks if the short link is cached in {@link JedisPool}. If the short link is found in the cache,
- * it retrieves the link directly from the cache; otherwise, it queries the {@link LinkService} to fetch
- * the link from the database. After retrieving the link, it updates its statistics and expiration time,
- * caches the link for future requests and redirects the user to the corresponding long link.
  *
  * @author Egor Sivenko
  * @see org.springframework.web.servlet.view.RedirectView
@@ -34,8 +23,6 @@ import java.time.LocalDateTime;
 public class LinkRedirectController {
 
     private final LinkService linkService;
-    private final JedisPool jedisPool;
-    private final ObjectMapper mapper;
 
     /**
      * Redirects a request with a short link to its corresponding long link.
@@ -46,37 +33,19 @@ public class LinkRedirectController {
     @SneakyThrows
     @GetMapping("/{shortLink}")
     public RedirectView redirectToOriginalLink(@PathVariable @NotBlank String shortLink) {
-        try (Jedis jedis = jedisPool.getResource()) {
-            Link link = jedis.exists(shortLink)
-                    ? mapper.readValue(jedis.get(shortLink), Link.class)
-                    : linkService.findByShortLink(shortLink);
-            updateLinkStats(link, jedis);
-            return redirectToLongLink(link);
-        }
+        String longLink = linkService.getLongLinkFromShortLink(shortLink);
+        return buildRedirectView(longLink);
     }
 
     /**
-     * Updates the link statistics, expiration time, and caches the link.
+     * Builds RedirectView to the provided url with 302 status code.
      *
-     * @param link the link to be updated
+     * @param url the url to redirect to
+     * @return a RedirectView object directing the user to the url
      */
-    @SneakyThrows
-    private void updateLinkStats(Link link, Jedis jedis) {
-        link.setStatistics(link.getStatistics() + 1);
-        link.setExpirationTime(LocalDateTime.now().plusMonths(1));
-
-        jedis.set(link.getShortLink(), mapper.writeValueAsString(link));
-    }
-
-    /**
-     * Redirects to the long link.
-     *
-     * @param link the link to redirect to
-     * @return a RedirectView object directing the user to the long link
-     */
-    private RedirectView redirectToLongLink(Link link) {
+    private RedirectView buildRedirectView(String url) {
         RedirectView redirectView = new RedirectView();
-        redirectView.setUrl(link.getLongLink());
+        redirectView.setUrl(url);
         redirectView.setStatusCode(HttpStatusCode.valueOf(302));
         return redirectView;
     }
